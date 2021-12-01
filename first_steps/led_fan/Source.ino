e0bc44ac17ef138c3b35f20d81bdfaee5a9dd3ad
CodeR-na-r1 <86515995+CodeR-na-r1@users.noreply.github.com>
2021-12-02 00:40:37 +0800

    add input with help COM port + add leds

--- first_steps/led_fan/Source.ino
+++ first_steps/led_fan/Source.ino
@@ -3,28 +3,25 @@
 #define COLOR_DEBTH 1   // кол-во байт на цвет
 
 #define NUMCOLORS 15   // кол-во цветов в массиве
-#define NUMMODES 4   // кол-во режимов подсветки в массиве (функции подсветок)
+#define NUMMODES 6   // кол-во режимов подсветки в массиве (функции подсветок)
 
 #include <microLED.h>
 microLED<NUMLEDS, STRIP_PIN, MLED_NO_CLOCK, LED_WS2812, ORDER_GRB, CLI_AVER, SAVE_MILLIS> strip;
 #include "leds_modes.h"
 #include "global_var.h"
 
-void (*modes_arr[])() = {led_off, static_color, random_leds, led_first};  // Массив с функциями подсветок
+void (*modes_arr[])() = {led_off, static_color, random_leds, animation_wheel, loading, led_first};  // Массив с функциями подсветок
 
 long last_time = millis();  // Переменная таймера
 
 static uint8_t now_mode;
 static uint8_t now_color;
-//volatile bool flag_interrupt;
-//volatile uint16_t count_interrupt;
 uint8_t* NOW_COLOR;
+
 void setup()
 {
   now_mode = 0;  // Переменная-счетчик текущего режима подсветки
   now_color = 0;  // Переменная-счетчик текущего цвета подсветки
-  //flag_interrupt = false;
-  //count_interrupt = 0;
   NOW_COLOR = &now_color;
 
   pinMode(3, INPUT_PULLUP);
@@ -45,49 +42,35 @@ void loop() {
   Serial.println("new_loop!");
   Serial.println(now_mode);
   Serial.println(now_color);
-  //Serial.println(count_interrupt);
   
   (*modes_arr[now_mode])();
-  //count_interrupt=0;
-  delay(1);
-  
-  // if (millis() - last_time >= 5000) // Сработывает раз в ~5 секунд
-  // {
-  //   last_time = millis();
-  //   Serial.println("5 seconds прошло");
-  // }
+  //delay(1000);
+
+  if (Serial.available() > 0) // Смена режима и цвета через порт (два числа через пробел)
+  {
+    if ((Serial.read() - '0') == 1) // Если первое == 1 ,то смена режима
+    {
+      now_mode = Serial.parseInt(); // Второе число == номер режима
+    }
+    else // Иначе смена цвета
+    {
+      now_color = Serial.parseInt(); // Второе число == номер подсветки
+    }
+  }
 }
 
 void change_mode()
 {
-  if (millis() - last_time >= 400)
+  if (millis() - last_time >= 200)
   {
     now_mode = ++now_mode >= NUMMODES ? 0 : now_mode;  //  Увелечение текущего режима подсветки или сброс при последней подсветке
     last_time = millis();
   }
-  
-  /*
-  if (!flag_interrupt && !count_interrupt)
-  {
-    flag_interrupt = true;
-    //last_time = millis();
-  }
-  if (count_interrupt <= 20 && flag_interrupt)
-  {
-    ++count_interrupt;
-  }
-  if (count_interrupt >= 20 && flag_interrupt)
-  {
-    flag_interrupt = false;
-    now_mode = ++now_mode >= NUMMODES ? 0 : now_mode;  //  Увелечение текущего режима подсветки или сброс при последней подсветке
-  }
-  Serial.println(count_interrupt);
-  */
 }
 
 void change_color()
 {
-  if (millis() - last_time >= 400)
+  if (millis() - last_time >= 200)
   {
     now_color = ++now_color >= NUMCOLORS ? 0 : now_color;  //  Увелечение текущего цвета подсветки или сброс при последнем цвете
     last_time = millis();
--- first_steps/led_fan/leds_modes.h
+++ first_steps/led_fan/leds_modes.h
@@ -1,7 +1,7 @@
 #include "global_var.h"
 //uint8_t* NOW_COLOR;
 
-mData colors[] = {mRed, mGreen, mYellow, mOrange, mLime, mMaroon, mOlive, mBlue, mNavy, mAqua, mMagenta, mTeal, mPurple, mSilver, mWhite}; 
+const mData colors[] = {mRed, mGreen, mYellow, mOrange, mLime, mMaroon, mOlive, mBlue, mNavy, mAqua, mMagenta, mTeal, mPurple, mSilver, mWhite}; 
 
 void led_off()
 {
@@ -32,6 +32,34 @@ void random_leds()
   strip.set(random() % NUMLEDS, colors[random() % NUMCOLORS]);
   delay(random() % 50);
   strip.show();
+  
+  return;
+}
+
+void animation_wheel()
+{
+  for (int j=0; j<256;j+=25)  // Сдвиг колеса hue
+  {
+    for (int i=0; i<NUMLEDS;++i)  // Цвета колеса hue
+  {
+    strip.set(i, mWheel((i*255/NUMLEDS + j)% 256));
+    strip.show();
+  }
+  delay(25);
+  }
+  
+  return;
+}
+
+void loading()
+{
+  for (int i = 0; i < NUMLEDS; ++i)  // По кругу только два диода
+  {
+    strip.set(i, colors[*NOW_COLOR]);
+    i>1 ? strip.set(i-2, mRGB(0, 0, 0)) : strip.set(NUMLEDS+i-2, mRGB(0, 0, 0));  // Тушим цвет диода, который предшествует двум горящим
+    delay(10);
+    strip.show();
+  }
   
   return;
 }
